7270bb4f66d899a8d6e8d4f1b79380d19ee9afa0
doomer888 <doomer888@protonmail.com>
2020-11-20 02:12:43 +0300

    fixing working with validators

--- src/main/java/utils/HitChecker.java
+++ src/main/java/utils/HitChecker.java
@@ -1,14 +1,14 @@
 package utils;
 
+import utils.validators.Validator;
+
 public class HitChecker {
     public static boolean isInArea(String firstVal, String secondVal, String thirdVal) {
         try {
 
-            thirdVal = Validator.handleRadiusValue(thirdVal);
-
             double x = Double.parseDouble(firstVal);
             double y = Double.parseDouble(secondVal);
-            double r = Double.parseDouble(thirdVal);
+            int r = Integer.parseInt(thirdVal);
 
             if ((x > -r / 2 && x < 0 && y > -r && y < 0)
                     || (x > 0 && y <= 0 && x * x + y * y < r / 2 * r / 2)
